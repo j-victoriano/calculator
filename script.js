2640daa6ffc42dea859b6c879be4db1c7dd79772
Julian <108022596+j-victoriano@users.noreply.github.com>
2025-01-11 11:57:56 -0800

    added variables to start functions

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const subtract = (a,b) => {
     return a - b;
 }
 
+const modulus = (a,b) => {
+    return a % b;
+}
+
 const multiply = (a,b) => {
     return a * b;
 }
@@ -22,6 +26,8 @@ const operate = (a, operator , b) => {
             return add(a,b);
         case '-':
             return subtract(a,b);
+        case '%':
+            return modulus(a,b);
         case '*':
             return multiply(a,b);
         case '/':
@@ -37,16 +43,64 @@ const operate = (a, operator , b) => {
 //Button Selection
 const button = document.querySelectorAll('input[type="button"]');
 const input = document.querySelector('input[type="text"]');
+
+const isOperator = (str) => {
+    return str === '+' || str === '%' || str === '-' || str ==='*'
+            || str === '/';
+}
+
+const isClear = (str) => {
+    return str === 'AC';
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+let step = 0;
+let result = 0;
+
+const numArray = [];
+const secondNumArray = [];
+
+
 button.forEach(btn => {
     btn.classList = "operation";
     //On Click, Add the value to form
     btn.addEventListener('click', e =>{
         // alert("oh hello!");
         // console.log("i hope this is working")
+        //Lets Try Creating an Object
         let buttonValue = btn.value;
-        input.value += buttonValue;
-        console.log(input.value);
+        if(!isOperator(buttonValue) && !isClear(buttonValue)){
+            console.log(operator);
+            // console.log(operator === null);
+            if(operator === null){
+                input.value += buttonValue;
+                a = input.value;
+                console.log("This is a value: ", a);
+            } else {
+                if (b === null) input.value = "";
+                console.log(input.value);
+                input.value += buttonValue;
+                b = input.value;
+                console.log("This is b value: ", b);                
+            }
+        } else if (isClear(buttonValue)){
+            const clear = document.querySelector("#clear");
+            clear.addEventListener('click', e => {
+                input.value = "";
+                console.log("Cleared Values in memory");
+            });
+        } else if (isOperator(buttonValue)) {
+            operator = buttonValue;
+            console.log(operator);
+            console.log("This is an operand ", operator);
+        }
     })
 });
+
+
+
+
 
 
