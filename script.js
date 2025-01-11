8c16afd73bd1ad40b48939f545ce5cb76c864548
Julian <108022596+j-victoriano@users.noreply.github.com>
2025-01-11 13:34:00 -0800

    add working operations

--- script.js
+++ script.js
@@ -53,14 +53,56 @@ const isClear = (str) => {
     return str === 'AC';
 }
 
+const isEquals = (btn) => {
+    return btn === '=';
+}
+
 let firstNumber;
 let secondNumber;
 let operator;
 let step = 0;
 let result = 0;
 
-const numArray = [];
-const secondNumArray = [];
+let numArray = [];
+let secondNumArray = [];
+
+const getNumber = (num) => {
+    if(step === 0 || step === 1){
+        numArray.push(num);
+        step = 1;
+        firstNumber = Number(numArray.join(''));
+        input.value = firstNumber;
+    } else if (step === 2){
+        secondNumArray.push(num);
+        secondNumber = Number(secondNumArray.join(''));
+        input.value = secondNumber;
+    }
+}
+
+const getOperator = (op) => {
+    step = 2;
+    operator = op;
+    document.getElementById('decimal').disabled = false;
+}
+
+const clear = () => {
+    input.value = 0;
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
+    step = 0;
+    numArray = [];
+    secondNumArray = [];
+    console.log("Cleared all values")
+}
+
+//Should clear second number to keep chaining operations
+const equate = () => {
+    firstNumber = result;
+    secondNumber = null;
+    secondNumArray = [];
+}
+
 
 
 button.forEach(btn => {
@@ -69,32 +111,24 @@ button.forEach(btn => {
     btn.addEventListener('click', e =>{
         // alert("oh hello!");
         // console.log("i hope this is working")
-        //Lets Try Creating an Object
-        let buttonValue = btn.value;
-        if(!isOperator(buttonValue) && !isClear(buttonValue)){
-            console.log(operator);
-            // console.log(operator === null);
-            if(operator === null){
-                input.value += buttonValue;
-                a = input.value;
-                console.log("This is a value: ", a);
-            } else {
-                if (b === null) input.value = "";
-                console.log(input.value);
-                input.value += buttonValue;
-                b = input.value;
-                console.log("This is b value: ", b);                
+        let btnValue = btn.value
+        if (!isOperator(btnValue) && !isClear(btnValue) && !isEquals(btnValue)){
+            console.log("This is not operator or clear btn");
+            if(btn.value === '.'){
+                document.getElementById('decimal').disabled = true;
             }
-        } else if (isClear(buttonValue)){
-            const clear = document.querySelector("#clear");
-            clear.addEventListener('click', e => {
-                input.value = "";
-                console.log("Cleared Values in memory");
-            });
-        } else if (isOperator(buttonValue)) {
-            operator = buttonValue;
-            console.log(operator);
-            console.log("This is an operand ", operator);
+            getNumber(btnValue);
+            console.log(`First Number: ${firstNumber} \nSecond Number: ${secondNumber}`);
+        } else if (isOperator(btnValue)){
+            getOperator(btnValue);
+            console.log(`Operator: ${operator}`);
+        } else if (isClear(btnValue)){
+            clear();
+        } else if (isEquals(btnValue)){
+            result = operate(firstNumber, operator, secondNumber);
+            console.log(result);
+            equate();
+            input.value = result;
         }
     })
 });
